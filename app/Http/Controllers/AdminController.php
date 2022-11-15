aaf42bdc522289db916c2b9817c2fd8facf20b7f
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-15 23:02:16 +0100

    define registerPage and dashboard methods

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,15 @@ class AdminController extends Controller
 {
 
     public function loginPage() {
-        return view();
+        return view('admin.login');
+    }
+
+    public function registerPage() {
+        return view('admin.register');
+    }
+
+    public function dashboard() {
+        return view('admin.dashboard');
     }
 
 }
