fdd06d3892c5665a469f3096e223b57bbc98bf03
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-16 22:05:36 +0100

    Define routes for login register post requests

--- routes/admin.php
+++ routes/admin.php
@@ -6,12 +6,21 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function() {
 
     Route::get('/login', [AdminController::class, 'loginPage'])
-            ->name('admin.login');
+            ->name('admin.login-page');
 
     Route::get('/register', [AdminController::class, 'registerPage'])
+            ->name('admin.register-page');
+
+    Route::post('/login', [AdminController::class, 'login'])
+            ->name('admin.login');
+
+    Route::post('/register', [AdminController::class, 'register'])
             ->name('admin.register');
 
+    Route::post('/logout', [AdminController::class, 'logout'])
+            ->name('admin.logout');
+
     Route::get('/dashboard', [AdminController::class, 'dashboard'])
-            ->name('admin.dashboard');
+            ->name('admin.dashboard')->middleware('admin');
 
 });
