400531ba6bce220e396a309d880f554f84df80c7
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-16 22:07:34 +0100

    Define 3 new methods login, register and logout each of them will handle post request

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,21 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
 
-    public function loginPage() {
-        return view('admin.login');
+    public function login(Request $request)
+    {
+        //return view('admin.login');
+        //dd($request->all());
+
+        $data = $request->all();
+
+        $check = Auth::guard('admin')->attempt([
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+
+        if ($check) {
+            return redirect()->route('admin.dashboard')->with('success', 'You\'ve logged in successfully');
+        }
+        return redirect()
+                ->route('admin.login-page')
+                ->with('error', 'Invalid credentials');
     }
 
-    public function registerPage() {
+    public function loginPage()
+    {
+        if (!Auth::check()) {
+            return view('admin.login');
+        }
+        return redirect()->route('admin.dashboard');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => [
+                'required',
+                'min:3',
+                'max:20'
+            ],
+            'email' => [
+                'required',
+                'unique:admins,email'
+            ],
+            'password' => [
+                'required',
+                'confirmed'
+            ]
+        ]);
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+        Admin::create($data);
+
         return view('admin.register');
     }
 
-    public function dashboard() {
-        return view('admin.dashboard');
+    public function registerPage()
+    {
+        return view('admin.register');
     }
 
+    public function logout() {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login')->with('success', 'You\'ve logged out successfully');
+    }
+
+    public function dashboard()
+    {
+        return view('admin.dashboard');
+    }
 }
