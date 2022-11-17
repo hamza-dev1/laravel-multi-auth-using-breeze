af13d9c28c501dc43f46ba181a976a131b0d1df5
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-17 13:48:17 +0100

    Author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorController extends Controller
+{
+
+    public function login(Request $request) {
+        $data = $request->all();
+        $check = Auth::guard('author')->attempt([
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+        if ($check) {
+            return redirect()
+                    ->route('author.dashboard')
+                    ->with('success', 'You\'ve logged in successfully');
+        }
+        return redirect()
+                ->route('author.login-page')
+                ->with('error', 'Invalid credentials');
+    }
+
+    public function loginPage()
+    {
+        return view('author.login');
+    }
+
+    public function register(Request $request) {
+
+    }
+
+    public function registerPage()
+    {
+        return view('author.register');
+    }
+
+    public function dashboard() {
+        return view('author.dashboard');
+    }
+
+}
