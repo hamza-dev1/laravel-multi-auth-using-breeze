38f8b42cc1bfc4baf0787ff1dacc6c4842e066f2
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-17 13:51:59 +0100

    Configure seeder to add 1 fake author

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
+use App\Models\Author;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Admin::factory(10)->create();
+        Admin::factory(1)->create();
+        Author::factory(1)->create();
     }
 }
