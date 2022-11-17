dfb7fabb38d35189c948522ce1a52268f5dfca20
hamza-dev1 <hamza.rquiq1@gmail.com>
2022-11-17 13:49:31 +0100

    Author routes php file

---
+++ routes/author.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('author')->group(function () {
+
+    Route::get('/login', [AuthorController::class, 'loginPage'])
+            ->name('author.login-page');
+
+    Route::get('/register', [AuthorController::class, 'registerPage'])
+            ->name('author.register-page');
+
+    Route::get('/dashboard', [AuthorController::class, 'dashboard'])
+            ->name('author.dashboard');
+
+    Route::post('/login', [AuthorController::class, 'login'])
+            ->name('author.login');
+
+    Route::post('/register', [AuthorController::class, 'register'])
+            ->name('author.register');
+});
